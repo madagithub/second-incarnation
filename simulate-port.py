e9954d47a3787c577c5b13f7320a86aa4857aab4
Idan Kimel <idan.kimel@gmail.com>
2020-12-15 04:38:49 +0200

    Update simulated port script

--- simulate-port.py
+++ simulate-port.py
@@ -4,17 +4,14 @@ master, slave = pty.openpty()
 name = os.ttyname(slave)
 print(f'Opening port on {name}...')
 
-input()
 print('Starting')
 
 while True:
-	print('Before read')
 	char = os.read(master, 1)
-	print(f'Read {char}')
 	if char == b'1':
 		print('Activating...')
 		os.write(master, bytes('1', 'utf-8'))
-		time.sleep(3)
+		time.sleep(30)
 		print('Deactivating...')
 		os.write(master, bytes('0', 'utf-8'))
 	if char == b'2':
